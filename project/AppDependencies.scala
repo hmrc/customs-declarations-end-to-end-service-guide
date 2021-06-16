59667986465d1904394fa2b5e24113095c86c0bc
Calin Ursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2021-06-16 09:03:32 +0100

    [CU][DLS-4286]: Library upgrades + Play 2.8 (#69)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,15 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "3.4.0"
+  val bootStrapPlayVersion = "5.3.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-27" % bootStrapPlayVersion)
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion,
+    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full),
+    "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full)
 
   val test = Seq(
     "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test,
-    "org.scalatest" %% "scalatest" % "3.2.0" % Test)
+    "org.scalatest" %% "scalatest" % "3.2.9" % Test)
 }
