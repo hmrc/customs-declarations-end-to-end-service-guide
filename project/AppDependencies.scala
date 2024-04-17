82e13b155093fd4393533ef56145497524c766f6
Edward Oscar Taylor <48755019+ed1286@users.noreply.github.com>
2024-04-17 10:12:05 +0100

    DCWL-2002  Play 3.0 upgrade (#261)
    * DCWL-2002 update appdependencies
    
    * DCWL-2002
    
    * DCWL-2002 update plugins
    
    * DCWL-2002 update build.sbt
    
    * DCWL-2002 update plugins
    
    * DCWL-2002 remove unnecessary config
    
    * DCWL-2002 removed brace

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,18 +1,18 @@
-import play.sbt.PlayImport._
-import sbt._
+import play.sbt.PlayImport.*
+import sbt.*
 
 object AppDependencies {
-  val bootStrapPlayVersion = "7.8.0"
+  val bootStrapVersion = "8.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion,
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootStrapVersion,
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootStrapPlayVersion % "test",
-    "org.pegdown"          % "pegdown"                  % "1.6.0"  % "test",
-    "com.vladsch.flexmark" % "flexmark-profile-pegdown" % "0.62.2" % "test",
-    "org.scalatest"        %% "scalatest"               % "3.2.11" % "test"
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootStrapVersion % Test,
+    "org.pegdown"          % "pegdown"                  % "1.6.0"  % Test,
+    "com.vladsch.flexmark" % "flexmark-profile-pegdown" % "0.64.8" % Test,
+    "org.scalatest"        %% "scalatest"               % "3.2.18" % Test
   )
 }
