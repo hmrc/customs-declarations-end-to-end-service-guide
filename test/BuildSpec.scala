832989dfd6d7b9c042bff075f4a3a2938837d0c6
Andrew Butters <23127331+abutters@users.noreply.github.com>
2024-11-06 11:03:03 +0000

    DCWL-2732 Fix Chrome console errors relating to assets. (#294)
    * DCWL-2732 Fix Chrome console errors relating to assets.

--- test/BuildSpec.scala
+++ test/BuildSpec.scala
@@ -16,15 +16,54 @@
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
+
+import java.io.{File, PrintWriter}
+import scala.io.{BufferedSource, Source}
 import scala.language.postfixOps
 import sys.process._
 
 class BuildSpec extends AnyWordSpec with Matchers {
+
+  private def readFileContent(filePath: String): String = {
+    val file: File             = new File(filePath)
+    val source: BufferedSource = Source.fromFile(file, "UTF-8")
+    try source.getLines().mkString("\n")
+    finally source.close()
+  }
+
+  private def updateFileContent(filePath: String, newContent: String): Unit = {
+    val file: File               = new File(filePath)
+    val printWriter: PrintWriter = new PrintWriter(file, "UTF-8")
+    try printWriter.write(newContent)
+    finally printWriter.close()
+  }
+
   "Building the content" should {
     "produce static files" in {
-      val result = "bundle install" #&& Process("bundle exec middleman build --build-dir=public/ --clean", None) !
+      val result: Int = "bundle install" #&& Process(
+        "bundle exec middleman build --build-dir=public/ --clean",
+        None
+      ) !
 
       result shouldBe 0
+    }
+  }
+
+  "Modifying the content" should {
+    "update all assets urls in manifest.css to start with the base route" in {
+      val filePath: String        = "public/stylesheets/manifest.css"
+      val originalContent: String = readFileContent(filePath)
+
+      updateFileContent(
+        filePath,
+        originalContent.replace("url(\"", "url(\"/guides/customs-declarations-end-to-end-service-guide")
+      )
+
+      val updatedContent: String = readFileContent(filePath)
+
+      updatedContent should not be originalContent
+
+      updatedContent should include("url(\"/guides/customs-declarations-end-to-end-service-guide")
     }
   }
 }
